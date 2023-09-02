2d6b82faf00a299e7647778d0e001cc8bfd414c3
rajathongal <reachme.rajathongal@gmail.com>
2023-09-02 20:24:33 +0530

    added more patterns

---
+++ patterns/InvertedNumericHalfPyramid.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cout << "Enter value of n" << endl;
+    cin >> n;
+
+    for(int row = 0; row < n; row = row + 1) {
+        for(int col = 0; col < n - row; col = col + 1) {
+            cout << col + 1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
