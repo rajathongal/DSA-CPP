9adcafc0302cb65f16147ded75a0396204582acf
rajathongal <reachme.rajathongal@gmail.com>
2024-03-09 14:39:01 +0530

    arrays l1

--- cpp_programming basics/practice.cpp
+++ cpp_programming basics/practice.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int Loops() {
+  int num = 700;
+
+  if (num % 2 == 0) {
+    cout << "Even" << endl;
+  } else {
+    cout << "Odd" << endl;
+  }
+
+  if (num > 0) {
+    cout << "Positive" << endl;
+  } else if (num < 0) {
+    cout << "Negative" << endl;
+  } else {
+    cout << "ZERO" << endl;
+  }
+
+  num = -20;
+
+  if (num > 0) {
+    cout << "Positive" << endl;
+  } else if (num < 0) {
+    cout << "Negative" << endl;
+  } else {
+    cout << "ZERO" << endl;
+  }
+
+  // infinite loop and break
+  int i = 1;
+  for (;;) {
+    cout << "Value of i is " << i << endl;
+    if (i < 30) {
+      cout << "Henlo Practice " << i << endl;
+      i++;
+    } else {
+      break;
+    }
+  }
+
+  // general loops
+  for (int i = 0; i < 10; i = i + 1) {
+    cout << i << endl;
+  }
+
+  for (int j = 20; j >= 0; j = j - 2) {
+    cout << j << endl;
+  }
+
+  return 0;
+}
+
+int Basic() {
   std::cout << "Hello world!" << std::endl;
   cout << "hello world from std namespace!" << endl;
 
@@ -35,12 +86,12 @@ int main() {
   cout << a % b << endl;
   cout << a / b << endl;
 
-  cout << ( a > b) << endl;
-  cout << ( a < b) << endl;
-  cout << ( a >= b ) << endl;
-  cout << ( a <= b) << endl;
-  cout << ( a != b ) << endl;
-  cout << ( a == b ) << endl;
+  cout << (a > b) << endl;
+  cout << (a < b) << endl;
+  cout << (a >= b) << endl;
+  cout << (a <= b) << endl;
+  cout << (a != b) << endl;
+  cout << (a == b) << endl;
 
   int marks;
 
@@ -48,6 +99,68 @@ int main() {
   cin >> marks;
 
   cout << "Here is your input" << endl << marks << endl;
+  return 0;
+}
 
+int Arrays() {
+  int a[7] = {0, 1, 2, 3, 4, 5, 6}; // static array
+  int arr[101];
+  int ch[101];
+  bool flags[90];
+  long nums[909];
+  short sh[1001];
+
+  int n = 10;
+  char cha[n];
+
+  for (int i = 0; i < n; i++) {
+    cout << "Enter char for index: " << i << endl;
+    cin >> cha[i];
+    cout << endl;
+  }
+
+  for (int i = 0; i < n; i++) {
+    cout << "Char entered at index " << i << " is: " << cha[i] << endl;
+  }
+
+  cout << "Size of array is " << sizeof(cha) << endl;
+
+  // explicit array init with empty values
+  int some[5] = {1, 2, 3};
+
+  // taking input in specified array index
+  cout << "enter value for array" << endl;
+  cin >> some[3];
+  some[4] = 2;
+  for (int i = 0; i < sizeof(some); i++) {
+    cout << some[i] << endl;
+  }
+
+  // linear search 
+  bool flag = false;
+  int ar[5] = {1,3,5,7,8};
+
+  for(int i=0; i < sizeof(ar); i++) {
+    if(ar[i] == 7) {
+      flag = true;
+      break;
+    }
+  }
+
+  if(flag == 1) {
+    cout << "target found" << endl;
+  } else {
+    cout << "target not found" << endl;
+  }
+
+
+
+  return 0;
+}
+
+int main() {
+  //   Basic();
+  //   Loops();
+  Arrays();
   return 0;
 }
