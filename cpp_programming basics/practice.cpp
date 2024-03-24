287220262eaf9ff80667169c8310d53db451abf7
rajathongal <reachme.rajathongal@gmail.com>
2024-03-24 16:32:06 +0530

    operators completed

--- cpp_programming basics/practice.cpp
+++ cpp_programming basics/practice.cpp
@@ -3,6 +3,8 @@
 #include <limits.h>
 using namespace std;
 
+int age = 20; // global variable
+
 // sizeof returns size of in bytes
 // to get the exact size of the array divide the value received from sizeof with
 // sizeof array's datatype
@@ -454,8 +456,39 @@ int Operators() {
   cout << "after post decr " << a << endl;
 
   // Variable Scoping
-  
 
+  // Local varaibles
+
+  // i is not accessble here
+  for (int i = 0; i < 6; i++) {
+    // i is a local variable for this for loop
+    cout << i << endl;
+  }
+  // i is not accessble here
+  if (true) {
+    int ab = 25; // ab is global scope for the inner if
+    if (true) {
+      cout << ab << endl; // ab is accessible here
+      // we can also redfine variable here
+
+      int ab = 100;
+
+      cout << ab << endl; // ab is now 100 only is second if loop level
+
+      // the nearest definition of variable ab is printed
+    }
+  }
+
+  a = 5; // updation
+
+  // redfinition of variable cannot be done
+  // int a = 100;
+
+  for (int i = 0; i < a + 1; i++) {
+    cout << i << a << endl; // a is accesible here as well
+  }
+
+  cout << a << endl; // a is accesible here as well
   return 0;
 }
 
