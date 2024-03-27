12c8a075f08a9639562abb6361d3cd035808d8ba
rajathongal <reachme.rajathongal@gmail.com>
2024-03-27 09:32:12 +0530

    hollow patterns added

--- cpp_programming basics/patternsPractice.cpp
+++ cpp_programming basics/patternsPractice.cpp
@@ -1,47 +1,91 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void square() {
 
-    int size;
-    cout << "Enter the value of size" << endl;
-    cin >> size;
+  int size;
+  cout << "Enter the value of size" << endl;
+  cin >> size;
 
-    //rows outer loop 
-    for(int row = 0; row < size; row++) {
-        // colmus inner loop
-        for(int col = 0; col < size; col++) {
-            cout << "* ";
-        }
-        cout << endl;
+  // rows outer loop
+  for (int row = 0; row < size; row++) {
+    // colmus inner loop
+    for (int col = 0; col < size; col++) {
+      cout << "* ";
     }
+    cout << endl;
+  }
 }
 
 void rectangle() {
-    int rows;
-    int cols;
+  int rows;
+  int cols;
 
-    cout << "Enter the number of rows" << endl;
-    cin >> rows;
-    cout << "Enter the number of columns" << endl;
-    cin >> cols;
+  cout << "Enter the number of rows" << endl;
+  cin >> rows;
+  cout << "Enter the number of columns" << endl;
+  cin >> cols;
 
-    // rows outer loop
-    for(int row = 0; row < rows; row++) {
-        // inner loop cols 
-        for(int col = 0; col < cols; col++) {
-            cout << "* ";
-        }
-        cout << endl;
+  // rows outer loop
+  for (int row = 0; row < rows; row++) {
+    // inner loop cols
+    for (int col = 0; col < cols; col++) {
+      cout << "* ";
     }
+    cout << endl;
+  }
 }
 
 void hollowSquare() {
-    
+  int size;
+  cout << "Enter the Size " << endl;
+  cin >> size;
+
+  // rows outer loop
+  for (int row = 0; row < size; row++) {
+    // cols inner loops
+    for (int cols = 0; cols < size; cols++) {
+      if (row == 0 || row == size - 1 || cols == 0 || cols == size - 1) {
+        cout << "* ";
+      } else {
+        cout << "  ";
+      }
+    }
+    cout << endl;
+  }
 }
 
-int main () {
-    // square();
-    // rectangle();
-    return 0;
+void hollowRectangle() {
+  int noOfRows;
+  int noOfCols;
+
+  cout << "Enter the number of rows" << endl;
+  cin >> noOfRows;
+  cout << "Enter the number of columns" << endl;
+  cin >> noOfCols;
+
+  // outer loop rows
+  for (int row = 0; row < noOfRows; row = row + 1) {
+    // inner for cols
+    for (int col = 0; col < noOfCols; col = col + 1) {
+      if (row == 0 || row == noOfRows - 1 || col == 0 || col == noOfCols - 1) {
+        cout << "* ";
+      } else {
+        cout << "  ";
+      }
+    }
+    cout << endl;
+  }
+}
+
+void HalfPyramid() {
+     
+}
+
+int main() {
+  // square();
+  // rectangle();
+  // hollowSquare();
+  // hollowRectangle();
+  return 0;
 }
