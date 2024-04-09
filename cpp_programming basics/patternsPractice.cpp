57e0aeafd2bcf5795d9c1aaf228351a53fa74d74
rajathongal <reachme.rajathongal@gmail.com>
2024-04-09 20:12:00 +0530

    fancy pattern three completed

--- cpp_programming basics/patternsPractice.cpp
+++ cpp_programming basics/patternsPractice.cpp
@@ -533,6 +533,28 @@ void FancyPatternTwo() {
   }
 }
 
+void FancyPatternThree() {
+  int size;
+  cout << "Enter Size of pattern" << endl;
+  cin >> size;
+
+  for (int row=0; row < size; row++) {
+    for(int col=0; col< size; col++) {
+      if(col ==0) {
+        cout << 1;
+      } else if(row == size - 1) {
+        cout << col + 1;
+      } else if(col == row + 1 && row != 0) {
+        cout << row + 1;
+      } else {
+        cout << " ";
+      }
+    }
+    cout << endl;
+
+  }
+}
+
 int main() {
   // square();
   // rectangle();
@@ -557,6 +579,7 @@ int main() {
   // HollowDiamond();
   // FlippedSolidDiamond();
   // FancyPatternOne();
-  FancyPatternTwo();
+  // FancyPatternTwo();
+  FancyPatternThree();
   return 0;
 }
