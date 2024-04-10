69dd4e4b10e052eb1fecb63a7d7f5f6ae031253a
rajathongal <reachme.rajathongal@gmail.com>
2024-04-10 16:47:34 +0530

    hollow numeric half pyramid

--- cpp_programming basics/patternsPractice.cpp
+++ cpp_programming basics/patternsPractice.cpp
@@ -538,16 +538,35 @@ void FancyPatternThree() {
   cout << "Enter Size of pattern" << endl;
   cin >> size;
 
-  for (int row=0; row < size; row++) {
-    for(int col=0; col< size; col++) {
-      if(col ==0) {
+  for (int row = 0; row < size; row++) {
+    for (int col = 0; col < size; col++) {
+      if (col == 0) {
         cout << 1;
-      } else if(row == size - 1) {
+      } else if (row == size - 1) {
         cout << col + 1;
-      } else if(col == row + 1 && row != 0) {
+      } else if (col == row + 1 && row != 0) {
         cout << row + 1;
       } else {
         cout << " ";
+      }
+    }
+    cout << endl;
+  }
+}
+
+void NumericHollowHalfPyramid() {
+  int size;
+  cout << "Enter Size of pattern" << endl;
+  cin >> size;
+
+  for (int row = 0; row < size; row++) {
+    for (int col = 0; col < row + 1; col++) {
+      if (col == 0) {
+        cout << 1 << " ";
+      } else if (row == col || row == size - 1) {
+        cout << col + 1 << " ";
+      } else {
+        cout << "  ";
       }
     }
     cout << endl;
@@ -580,6 +599,7 @@ int main() {
   // FlippedSolidDiamond();
   // FancyPatternOne();
   // FancyPatternTwo();
-  FancyPatternThree();
+  // FancyPatternThree();
+  NumericHollowHalfPyramid();
   return 0;
 }
