89feebe3d700a32ee6614aeaf54e3de8a4ab43ad
rajathongal <reachme.rajathongal@gmail.com>
2024-04-23 21:39:14 +0530

    debug problem solutions

--- cpp_basics/debugProblems.cpp
+++ cpp_basics/debugProblems.cpp
@@ -1,6 +1,6 @@
+#include <climits>
 #include <cmath>
 #include <iostream>
-
 using namespace std;
 
 void AreaOfRectangle() {
@@ -82,11 +82,128 @@ bool checkMember(int n) {
   }
 }
 
+/*
+Pattern for N = 4
+   1
+  212
+ 32123
+4321234
+*/
+
+void PyramidPattern() {
+  int n;
+  cin >> n;
+  int i = 0, gaps = n - 1;
+  while (i < n) {
+    int k = 0, gaps = n - 1 - i;
+    while (k < gaps) {
+      cout << " ";
+      k++;
+    }
+    int num = i + 1;
+    while (num > 0) {
+      cout << num;
+      num = num - 1;
+    }
+    k = 2;
+    while (k <= (i + 1)) {
+      cout << k;
+      k++;
+    }
+    k = 0;
+    while (k < gaps) {
+      cout << " ";
+      k++;
+    }
+    i++;
+    cout << "\n";
+  }
+}
+
+/*
+Pattern for N = 5
+1234554321
+1234**4321
+123****321
+12******21
+1********1
+*/
+
+void Pattern() {
+  int n;
+  cin >> n;
+  int i = 0;
+  while (i < n) {
+    int count = n - i, k = 1;
+    while (k <= count) {
+      cout << k;
+      k++;
+    }
+    count = n - i;
+    int stars = 2 * i;
+    while (stars > 0) {
+      cout << "*";
+      stars--;
+    }
+    count = n - i;
+    while (count > 0) {
+      cout << count;
+      count--;
+    }
+    cout << "\n";
+    i++;
+  }
+}
+
+void SecondLargestNumber() {
+  int n;
+  cin >> n;
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  if (n == 0 || n == 1) {
+    cout << INT_MIN;
+  } else {
+    int max = INT_MIN;
+    for (int i = 0; i < n; i++) {
+      if (max < arr[i]) {
+        max = arr[i];
+      }
+    }
+    int second = INT_MIN;
+    for (int i = 0; i < n; i++) {
+      if (max > arr[i] && arr[i] > second) {
+        second = arr[i];
+      }
+    }
+    cout << second;
+  }
+}
+
+void Pattern2(){
+  int i,j,n;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=i;j<=n;j++){
+            cout<<n-i+1;
+            
+        }
+        cout<<endl;
+        
+    }
+}
+
+
 int main() {
   // AreaOfRectangle();
   // binaryToDecimal();
   // PrintNPrimeNumbers();
   // ReverseNumber();
-  // cout << checkMember(7) << endl << checkMember(593) << endl << checkMember(21) << endl;
+  // cout << checkMember(7) << endl << checkMember(593) << endl <<
+  // checkMember(21) << endl; PyramidPattern(); Pattern();
+  // SecondLargestNumber();
+  Pattern2();
   return 0;
 }
