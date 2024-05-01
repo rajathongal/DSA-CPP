a242d2dd18b70852b2d9f5854707f26f67cb31d8
rajathongal <reachme.rajathongal@gmail.com>
2024-05-01 15:48:18 +0530

    ref var

--- cpp_basics/functions/passByReference.cpp
+++ cpp_basics/functions/passByReference.cpp
@@ -1,25 +1,60 @@
 #include <iostream>
 using namespace std;
 
-void solve(int& a) { // get actual varibale address and any operation made here can be seen in main function as well 
-    a++;
-    cout << "Print inside solve " << a << endl;
+void referenceVariable() {
+  // variable
+  int number = 100;
+
+  // reference variable
+  int &k = number;
+
+  // reference variable
+  int &c = number;
+
+  cout << "number: " << number << endl;
+  cout << "k: " << k << endl;
+  cout << "c: " << c << endl;
+
+  // if original value of original variable is modified then
+  // reference variables value will also be modified
+  number += 99;
+
+  cout << "number: " << number << endl;
+  cout << "k: " << k << endl;
+  cout << "c: " << c << endl;
+
+  // any reference variable modified will also affect original variable
+  k++;
+
+  cout << "number: " << number << endl;
+  cout << "k: " << k << endl;
+  cout << "c: " << c << endl;
+
+  // int &kt = 6; this will throw error as reference variables do not take contants or direct inputs 
+  // but take only reference of an existing variable.
 }
 
-void solve2(int& a) {
-    a--;
-    a = a * 10;
-    cout << "Print inside solve " << a << endl;
+void solve(int &a) {
+  // get actual varibale address and any operation made here
+  // can be seen in main function as well
+  a++;
+  cout << "Print inside solve " << a << endl;
+}
+
+void solve2(int &a) {
+  a--;
+  a = a * 10;
+  cout << "Print inside solve " << a << endl;
 }
 
 int main() {
-    int a = 5;
-    solve(a);
-    cout << "Print inside main "<<  a << endl;
+  int a = 5;
+  solve(a);
+  cout << "Print inside main " << a << endl;
 
-    a++;
-    solve2(a);
-    cout << "Print inside main "<<  a << endl;
+  a++;
+  solve2(a);
+  cout << "Print inside main " << a << endl;
 
+  referenceVariable();
 }
-
