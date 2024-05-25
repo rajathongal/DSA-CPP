216958366918d6dc47568b86c3c567445f3fb5f6
rajathongal <reachme.rajathongal@gmail.com>
2024-05-25 22:24:14 +0530

    find peak element and missing element added

--- searchingAndSorting/binarySearch/basicBinarySearch.cpp
+++ searchingAndSorting/binarySearch/basicBinarySearch.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void basicBinarySearch() {
@@ -89,10 +90,76 @@ void findTotalOccurance() {
   cout << "Totol Occurance is: " << totalOccurance << endl;
 }
 
+void findMissingElement(int arr[], int size) {
+  int start = 0;
+  int end = size - 1;
+  int mid = start + (end - start) / 2;
+  int ans = -1;
+
+  while (start <= end) {
+    int difference = arr[mid] - mid;
+    if (difference == 1) {
+      start = mid + 1;
+    } else {
+      ans = mid;
+      end = mid - 1;
+    }
+
+    mid = start + (end - start) / 2;
+  }
+
+  // try to exclude below statement and solve it in above logic HW
+  if (ans + 1 == 0) {
+    cout << "Missing Element is: " << size + 1 << endl;
+    return;
+  }
+
+  cout << "Missing Element is: " << ans + 1 << endl;
+  return;
+}
+
+// Leetcode 852
+// find peak indes in a mountain array
+// input [0,1,0]
+// output 1
+// solve in TC of O(logn)
+void findPeakIndexInAMountainArray(int arr[], int size) {
+  int start = 0;
+  int end = size - 1;
+  int mid = start + (end - start) / 2;
+
+  while (start < end) {
+    if (arr[mid] < arr[mid + 1]) {
+      // in A section of graph
+      // peak exists right side
+      // so move right
+      start = mid + 1;
+    } else {
+      // you might be in B section of graph
+      // and you might be on peak element
+      // move left without losing peak element so end = mid
+      end = mid;
+    }
+
+    // update mid
+    mid = start + (end - start) / 2;
+  }
+
+  cout << "Peak element is: " << start << endl;
+}
+
 int main() {
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8}; // for missing elem
+  int size = 9;                         // for missing elem
+
+  int arrA[] = {0, 2, 1, 0}; // for peak index
+  int sizeA = 4;             // for peak index
+
   // basicBinarySearch();
   // findFirstOccurance();
   // findLastOccurance();
-  findTotalOccurance();
+  // findTotalOccurance();
+  // findMissingElement(arr, size);
+  findPeakIndexInAMountainArray(arrA, sizeA);
   return 0;
 }
