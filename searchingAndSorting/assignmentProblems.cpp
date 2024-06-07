52f73b262c6fe19550f3629285cc4a7b99faf1ad
rajathongal <reachme.rajathongal@gmail.com>
2024-06-07 21:48:07 +0530

    k closest elemnets using binary search

--- searchingAndSorting/assignmentProblems.cpp
+++ searchingAndSorting/assignmentProblems.cpp
@@ -116,15 +116,68 @@ void findKClosestElementsUsingTwoPointers() {
   int low = 0;
   int high = nums.size() - 1;
 
-  while(high - low >= k) {
-    if(x - nums[low] > nums[high] - x) {
+  while (high - low >= k) {
+    if (x - nums[low] > nums[high] - x) {
       low++;
     } else {
       high--;
     }
   }
 
-  for(int i=low; i<=high; i++) {
+  for (int i = low; i <= high; i++) {
+    cout << nums[i] << " ";
+  }
+
+  cout << endl;
+}
+
+// k closest element using Binary Search
+int lowerBound(vector<int> &nums, int target) {
+  int start = 0, end = nums.size() - 1;
+  int ans = end; // overflow
+
+  while (start <= end) {
+    int mid = start + (end - start) / 2;
+
+    if (nums[mid] >= target) {
+      ans = mid;
+      end = mid - 1;
+    } else if (target > nums[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  return ans;
+}
+
+void findKClosestElementsUsingBinarySearch() {
+  vector<int> nums;
+  nums.push_back(3);
+  nums.push_back(5);
+  nums.push_back(8);
+  nums.push_back(10);
+
+  int k = 2;
+  int x = 15;
+
+  int high = lowerBound(nums, x);
+  int low = high - 1;
+
+  while (k--) {
+    if (low < 0) {
+      high++;
+    } else if (high >= nums.size()) {
+      low--;
+    } else if (x - nums[low] > nums[high] - x) {
+      high++;
+    } else {
+      low--;
+    }
+  }
+
+  for (int i = low + 1; i < high; i++) {
     cout << nums[i] << " ";
   }
 
@@ -134,6 +187,7 @@ void findKClosestElementsUsingTwoPointers() {
 int main() {
   // kDiffPairsUsingTwoPointer();
   // kDiffPairsUsingBinarySearch();
-  findKClosestElementsUsingTwoPointers();
+  // findKClosestElementsUsingTwoPointers();
+  findKClosestElementsUsingBinarySearch();
   return 0;
 }
