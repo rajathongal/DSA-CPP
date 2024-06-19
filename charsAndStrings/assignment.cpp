5c3a3c0ae8a980508fddb5897f1539182ac77f8a
rajathongal <reachme.rajathongal@gmail.com>
2024-06-19 22:26:27 +0530

    largest number from given numbers

--- charsAndStrings/assignment.cpp
+++ charsAndStrings/assignment.cpp
@@ -418,6 +418,35 @@ void convert(string s, int numRows) {
   cout << endl;
 }
 
+// Leetcode 179. Largest Number
+
+static bool comparator(string a, string b) {
+  string t1 = a + b;
+  string t2 = b + a;
+
+  return t1 > t2;
+}
+
+void largestNumber(vector< int > &nums) {
+  vector< string > snums;
+  for (auto n : nums) {
+    snums.push_back(to_string(n));
+  }
+
+  sort(snums.begin(), snums.end(), comparator);
+
+  string answer = "";
+  if (snums[0] == "0") {
+    cout << "0" << endl;
+    return;
+  }
+  for (auto str : snums) {
+    answer += str;
+  }
+
+  cout << answer << endl;
+}
+
 int main() {
   // isAnagram("anagram", "nagaram");
   // isAnagram("rat", "car");
@@ -524,9 +553,37 @@ int main() {
   // intToRoman(1994);
   // intToRoman(1999);
 
-  convert("PAYPALISHIRING", 3);
-  convert("PAYPALISHIRING", 4);
-  convert("A", 1);
+  // convert("PAYPALISHIRING", 3);
+  // convert("PAYPALISHIRING", 4);
+  // convert("A", 1);
+
+  // for largest number
+  vector< int > numsTestCaseOne;
+  vector< int > numsTestCaseTwo;
+  vector< int > numsTestCaseThree;
+
+
+  numsTestCaseOne.push_back(10);
+  numsTestCaseOne.push_back(2);
+
+  numsTestCaseTwo.push_back(3);
+  numsTestCaseTwo.push_back(30);
+  numsTestCaseTwo.push_back(34);
+  numsTestCaseTwo.push_back(5);
+  numsTestCaseTwo.push_back(9);
+
+  numsTestCaseThree.push_back(0);
+  numsTestCaseThree.push_back(0);
+  numsTestCaseThree.push_back(0);
+  numsTestCaseThree.push_back(0);
+  numsTestCaseThree.push_back(0);
+  numsTestCaseThree.push_back(0);
+
+
+  largestNumber(numsTestCaseOne);
+  largestNumber(numsTestCaseTwo);
+  largestNumber(numsTestCaseThree);
+
 
   return 0;
 }
