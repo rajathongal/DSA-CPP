dfd9f62bb0e72482eccc9f2d44bf62c4b3257736
rajathongal <reachme.rajathongal@gmail.com>
2024-06-27 14:15:54 +0530

    fib series sum and 2 pow

--- recursion/level1.cpp
+++ recursion/level1.cpp
@@ -33,8 +33,33 @@ void printReverseCount(int n) {
   printReverseCount(n - 1);
 }
 
+int pow(int n) {
+  // base case
+  // base case
+  if (n == 0 ) {
+    return 1;
+  }
+
+  int ans = 2 * pow(n-1);
+  return ans;
+}
+
+int NfibonacciSeriesSum(int n) {
+  if(n == 0) {
+    return 0;
+  } 
+  if(n == 1) {
+    return 1;
+  } 
+
+  int ans = NfibonacciSeriesSum(n-1) + NfibonacciSeriesSum(n-2);
+  return ans;
+}
+
 int main() {
   // cout << factorial(5) << endl;
-  printReverseCount(20);
+  // printReverseCount(20);
+  // cout << pow(4) << endl;
+  cout << NfibonacciSeriesSum(4) << endl;
   return 0;
 }
