3718b9390eff20c342b0f60932dc1d0a2d793a90
rajathongal <reachme.rajathongal@gmail.com>
2024-06-30 10:03:05 +0530

    recursion level2 completed

--- recursion/level2.cpp
+++ recursion/level2.cpp
@@ -102,11 +102,87 @@ void doubleTheNumInArray(int arr[], int size, int index) {
   doubleTheNumInArray(arr, size, index + 1);
 }
 
+int searchArrayIndex(int arr[], int size, int target, int index) {
+  if (index >= size) {
+    return -1;
+  }
+
+  if (arr[index] == target) {
+    return index;
+  }
+
+  return searchArrayIndex(arr, size, target, index + 1);
+}
+
+void printAllOccurancesOfTargetInArrray(int arr[], int size, int target,
+                                        int index) {
+  if (index >= size) {
+    return;
+  }
+
+  if (arr[index] == target) {
+    cout << index << " ";
+  }
+
+  printAllOccurancesOfTargetInArrray(arr, size, target, index + 1);
+}
+
+void findAllOccurancesOfTargetInArrray(int arr[], int size, int target,
+                                       int index, vector< int > &ans) {
+
+  if (index >= size) {
+    return;
+  }
+
+  if (arr[index] == target) {
+    ans.push_back(index);
+  }
+
+  findAllOccurancesOfTargetInArrray(arr, size, target, index + 1, ans);
+
+  return;
+}
+
+void getDigitsFromNum(int num, vector< int > &digits) {
+  if (num <= 0) {
+    return;
+  }
+  int digit = num % 10;
+  num = num / 10;
+
+  getDigitsFromNum(num, digits);
+  digits.push_back(digit);
+  // cout << digit << " ";
+}
+
+void digitToNumber(vector< int > &digits, int size, int index, int &ans) {
+  if (index >= size) {
+    return;
+  }
+
+  ans = ans * 10 + digits[index];
+  digitToNumber(digits, size, index + 1, ans);
+}
+
+void printTargetCharIndex(string s, int index, char target,
+                          vector< int > &ans) {
+  if (s[index] == '\0') {
+    return;
+  }
+
+  if (s[index] == target) {
+    // cout << index << endl;
+    ans.push_back(index);
+  }
+
+  printTargetCharIndex(s, index + 1, target, ans);
+}
+
 int main() {
   // cout << climbStairs(4) << endl;
 
   // for print array and search array, minNum in array, even num in array
-  int arr[] = {11, 23, 30, 47, 93, 60, 70};
+  int arr[] = {11, 60, 23, 30, 60, 47, 93, 60, 70};
   int sizeOfArr = sizeof(arr) / sizeof(arr[0]);
   int target = 60;
 
@@ -125,8 +201,29 @@ int main() {
   // doubleTheNumInArray(arr, sizeOfArr, 0);
   // printArray(arr, sizeOfArr, 0);
 
-  int maxNum = INT_MIN;
-  maxNumInArr(arr, sizeOfArr, 0, maxNum);
-  cout << maxNum << endl;
+  // int maxNum = INT_MIN;
+  // maxNumInArr(arr, sizeOfArr, 0, maxNum);
+  // cout << maxNum << endl;
+
+  // cout << searchArrayIndex(arr, sizeOfArr, target, 0) << endl;
+
+  // printAllOccurancesOfTargetInArrray(arr, sizeOfArr, target, 0);
+
+  // vector<int> anss;
+  // findAllOccurancesOfTargetInArrray(arr, sizeOfArr, target, 0, anss);
+  // printVector(anss);
+
+  // vector<int> digits;
+  // getDigitsFromNum(559090, digits);
+  // printVector(digits);
+
+  // int num = 0;
+  // digitToNumber(digits, digits.size(), 0, num);
+  // cout << "Digit to num " << num << endl;
+
+  vector< int > targetPosOfString;
+  printTargetCharIndex("Rajatadsdsaa", 0, 'a', targetPosOfString);
+  printVector(targetPosOfString);
+
   return 0;
 }
