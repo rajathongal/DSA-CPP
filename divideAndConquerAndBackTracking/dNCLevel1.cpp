a2ffa471e5e2221ead51786f6ebf7f9a159fa1cd
rajathongal <reachme.rajathongal@gmail.com>
2024-07-07 11:40:54 +0530

    added quick sort considering last element as pivot

--- divideAndConquerAndBackTracking/dNCLevel1.cpp
+++ divideAndConquerAndBackTracking/dNCLevel1.cpp
@@ -16,7 +16,7 @@ void printArray(int arr[], int size) {
 // RE call of left half T(n/2)
 // RE call of right half T(n/2)
 // merge -> O(n) or k*n
-// SC -> 
+// SC ->
 
 void merge(int arr[], int start, int end) {
   // TC for creation is constant here
@@ -24,7 +24,7 @@ void merge(int arr[], int start, int end) {
   int lengthOfLeftArray = mid - start + 1;
   int lengthOfRightArray = end - mid;
 
-  // create left and right array 
+  // create left and right array
   // TC is n/2 here
   int *left = new int[lengthOfLeftArray];
   int *right = new int[lengthOfRightArray];
@@ -105,15 +105,55 @@ void mergeSort(int arr[], int start, int end) {
   merge(arr, start, end);
 }
 
+// quick sort
+// consider end as pivot always
+// sort in asc order such that
+// left of pivot is less than pivot
+// right of pivot is greater than pivot
+// TC -> O(n^2)
+void quickSort(int a[], int start, int end) {
+  if (start >= end) {
+    return;
+  }
+  int pivot = end;
+  int i = start - 1;
+  int j = start;
+
+  while (j < pivot) {
+    if (a[j] < a[pivot]) {
+      ++i;
+      swap(a[i], a[j]);
+    }
+    ++j;
+  }
+  ++i;
+  // i is the right position for pivot the pivot element
+  swap(a[i], a[pivot]);
+  quickSort(a, start, i - 1);
+  quickSort(a, i + 1, end);
+}
+
 int main() {
   int arr[] = {2, 1, 6, 9, 4, 5};
   int size = sizeof(arr) / sizeof(arr[0]);
   int start = 0;
   int end = size - 1;
+
   cout << "Before merge Sort" << endl;
   printArray(arr, size);
   mergeSort(arr, start, end);
   cout << "After merge Sort" << endl;
   printArray(arr, size);
+
+  int arr2[] = {7, 2, 1, 8, 6, 3, 5, 4};
+  int size2 = sizeof(arr2) / sizeof(arr2[0]);
+  int start2 = 0;
+  int end2 = size2 - 1;
+  cout << "Before quick Sort" << endl;
+  printArray(arr2, size2);
+  quickSort(arr2, start2, end2);
+  cout << "After quick Sort" << endl;
+  printArray(arr2, size2);
+
   return 0;
 }
