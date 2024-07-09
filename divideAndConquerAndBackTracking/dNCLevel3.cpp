f006ff944c82c4f89cdd8d931222d6b17c378438
rajathongal <reachme.rajathongal@gmail.com>
2024-07-09 15:49:30 +0530

    dearrangement

--- divideAndConquerAndBackTracking/dNCLevel3.cpp
+++ divideAndConquerAndBackTracking/dNCLevel3.cpp
@@ -17,7 +17,7 @@ int rob(vector< int > &nums, int size, int index) {
 }
 
 int robRobber2(vector< int > &nums, int size, int index) {
-  if(size == 1) {
+  if (size == 1) {
     return nums[0];
   }
   int option1 = rob(nums, size - 2, 0);
@@ -26,17 +26,47 @@ int robRobber2(vector< int > &nums, int size, int index) {
   return finalAns;
 }
 
+// Count DeArrangement
+// https://www.geeksforgeeks.org/count-derangements-permutation-such-that-no-element-appears-in-its-original-position/
+int countDeArrangement(int n) {
+  // base case
+  if (n == 1)
+    return 0;
+  if (n == 2)
+    return 1;
+
+  // Processing
+  int ans = (n - 1) * (countDeArrangement(n - 1) + countDeArrangement(n - 2));
+  return ans;
+}
+
+// painting fence algo
+// https://www.geeksforgeeks.org/painting-fence-algorithm/
+// Returns count of ways to color k posts
+long countWays(int n, int k) {
+  
+}
+
 int main() {
-  vector< int > nums1 = {2, 3, 2};
-  vector< int > nums2 = {1, 2, 3, 1};
-  vector< int > nums3 = {1, 2, 3, 4};
-  vector< int > nums4 = {10};
+  // vector< int > nums1 = {2, 3, 2};
+  // vector< int > nums2 = {1, 2, 3, 1};
+  // vector< int > nums3 = {1, 2, 3, 4};
+  // vector< int > nums4 = {10};
 
-  cout << robRobber2(nums1, nums1.size(), 0) << endl;
-  cout << robRobber2(nums2, nums2.size(), 0) << endl;
-  cout << robRobber2(nums3, nums3.size(), 0) << endl;
-  cout << robRobber2(nums4, nums4.size(), 0) << endl;
+  // cout << robRobber2(nums1, nums1.size(), 0) << endl;
+  // cout << robRobber2(nums2, nums2.size(), 0) << endl;
+  // cout << robRobber2(nums3, nums3.size(), 0) << endl;
+  // cout << robRobber2(nums4, nums4.size(), 0) << endl;
 
+  // cout << countDeArrangement(5) << endl;
+  // cout << countDeArrangement(4) << endl;
+  // cout << countDeArrangement(3) << endl;
+  // cout << countDeArrangement(1) << endl;
+
+  cout << countWays(2, 4) << endl;
+  cout << countWays(3, 2) << endl;
 
   return 0;
 }
+
+// catlan series must read
