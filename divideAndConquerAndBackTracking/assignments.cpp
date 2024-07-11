a1c12a1b5fabf830a1228dc7b4615e2c64f0b4a4
rajathongal <reachme.rajathongal@gmail.com>
2024-07-11 21:51:20 +0530

    mergesort two sorted arrays btdnc assignment

--- divideAndConquerAndBackTracking/assignments.cpp
+++ divideAndConquerAndBackTracking/assignments.cpp
@@ -119,6 +119,39 @@ void sortArray(vector< int > &nums) {
   mergeSortInPlace(nums, 0, nums.size() - 1);
   return;
 }
+
+// Merge 2 sorted Arrays
+void merge2SortedArrays(vector< int > &a, vector< int > &b) {
+  int n = a.size();
+  int m = b.size();
+  int total_len = n + m;
+  int gap = (total_len / 2) + total_len % 2;
+  while (gap > 0) {
+    int i = 0, j = gap;
+    while (j < total_len) {
+      // comparing elements in first array - if b[f]<a(i] swap their values
+      if (j < n && a[i] > a[j]) {
+        swap(a[i], a[j]);
+      } else if (j >= n && i < n && a[i] > b[j - n]) {
+        /* comparing elements
+      in both
+      arrays */
+        swap(a[i], b[j - n]);
+      }
+
+      /* comparing elements
+        in the second array */
+      else if (j >= n && i >= n && b[i - n] > b[j - n]) {
+        swap(b[i - n], b[j - n]);
+      }
+
+      j++;
+      i++;
+    }
+    gap = gap <= 1 ? 0 : (gap / 2) + (gap & 2);
+  }
+}
+
 int main() {
   // vector< int > arrOne = {8,4,2,1};
   // cout << countInversion(arrOne) << endl;
