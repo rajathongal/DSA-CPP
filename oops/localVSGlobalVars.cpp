1bc177fc6dd5ca741940e8e80a74b41d14015e7e
rajathongal <reachme.rajathongal@gmail.com>
2024-07-13 09:29:01 +0530

    Distribute repeating integers Leetcode 1655 btdnc assignment

---
+++ oops/localVSGlobalVars.cpp
@@ -0,0 +1,19 @@
+// Global Variable -
+// written out side a function or a class
+// accessible to all functions with same copy or same memory address
+
+// Local Variable -
+// written inside a function
+// accessible inside the function scope only
+// scoped
+
+#include <iostream>
+using namespace std;
+
+int x = 2; // Global Variable 
+
+int main() {
+  cout << x << endl;
+  return 0;
+
+}
