feabe0434f480192f8873897a1063d426cc51802
rajathongal <reachme.rajathongal@gmail.com>
2025-03-15 19:41:17 +0530

    added celebrity problem

--- stacks/assignments.cpp
+++ stacks/assignments.cpp
@@ -57,6 +57,61 @@ string removeDuplicates(string s) {
   return ans;
 }
 
+// https://www.geeksforgeeks.org/problems/the-celebrity-problem/1
+int celebrity(vector< vector< int > > &mat) {
+  int n = mat.size();
+  if (n == 0)
+    return -1;
+  stack< int > st;
+
+  // Push all persons into stack
+  for (int i = 0; i < n; ++i) {
+    st.push(i);
+  }
+
+  // Run Discard Method, to get a mightBeCelebrity
+  while (st.size() != 1) {
+    // pick top two elements and compare
+    int a = st.top();
+    st.pop();
+    int b = st.top();
+    st.pop();
+
+    // If a know b ?
+    if (mat[a][b]) {
+      // a is not celebrity, b might be
+      st.push(b);
+    } else {
+      st.push(a);
+    }
+  }
+
+  // If stack is empty, return -1
+  if (st.empty())
+    return -1;
+
+  // Check that single person is actually celebrity
+  int mightBeCelebrity = st.top();
+  st.pop();
+
+  // Celebrity Should not know anyone
+  // all rows should be zero
+  for (int i = 0; i < n; i++) {
+    if (mat[mightBeCelebrity][i] != 0) {
+      return -1;
+    }
+  }
+  //  Everybody Should know Celebrity
+  for (int i = 0; i < n; i++) {
+    if (mat[i][mightBeCelebrity] == 0 && i != mightBeCelebrity) {
+      return -1;
+    }
+  }
+
+  // mightBeCelebrity is the Celebrity
+  return mightBeCelebrity;
+} // TC O(N)
+
 int main() {
   // Uncomment for count reversal problem
   // string s1 = "}{{}}{{{";
@@ -65,10 +120,23 @@ int main() {
   // cout << "S2: " << countRev(s2) << endl;
 
   // Uncomment for leetcode 1047 Remove all adjacent duplicates in string
-  string s1 = "abbaca";
-  string s2 = "azxxzy";
+  // string s1 = "abbaca";
+  // string s2 = "azxxzy";
 
-  cout << "S1: " << s1 << " -> " << removeDuplicates(s1) << endl;
-  cout << "S2: " << s2 << " -> " << removeDuplicates(s2) << endl;
+  // cout << "S1: " << s1 << " -> " << removeDuplicates(s1) << endl;
+  // cout << "S2: " << s2 << " -> " << removeDuplicates(s2) << endl;
+
+  // Uncomment for The Celebrity Problem
+  // https://www.geeksforgeeks.org/problems/the-celebrity-problem/1
+  vector< vector< int > > matOne = {{1, 1, 0}, {0, 1, 0}, {0, 1, 1}};
+  vector< vector< int > > matTwo = {{1, 1}, {1, 1}};
+  vector< vector< int > > matThree = {{0, 1, 0}, {0, 0, 0}, {0, 1, 0}};
+  vector< vector< int > > matFour = {{0, 0, 1}, {1, 0, 1}, {0, 0, 0}};
+
+  cout << "The Celebrity is (matOne): " << celebrity(matOne) << endl;
+  cout << "The Celebrity is (matTwo): " << celebrity(matTwo) << endl;
+  cout << "The Celebrity is (matThree): " << celebrity(matThree) << endl;
+  cout << "The Celebrity is (matFour): " << celebrity(matFour) << endl;
+
   return 0;
 }
