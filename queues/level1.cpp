031ca58bf507bc7064f618be2aa298f8c2257e70
rajathongal <reachme.rajathongal@gmail.com>
2025-03-22 22:18:37 +0530

    level1:custom Queue

--- queues/level1.cpp
+++ queues/level1.cpp
@@ -2,6 +2,91 @@
 #include <queue>
 using namespace std;
 
+class CustomQueueUsingArray {
+public:
+  int *arr; // dynamic arr
+  int size;
+  int front;
+  int rear;
+
+  CustomQueueUsingArray(int size) {
+    arr = new int[size];
+    this->size = size;
+    front = -1;
+    rear = -1;
+  }
+
+  void push(int val) {
+    // check full
+    if (rear == size - 1) {
+      cout << "Queue Overflow" << endl;
+      return;
+    } else if (front == -1 && rear == -1) {
+      // empty case
+      front++;
+      rear++;
+      arr[rear] = val;
+      return;
+    } else {
+      // base case
+      rear++;
+      arr[rear] = val;
+    }
+  }
+
+  void pop() {
+    // underflow
+    if (front == -1 && rear == -1) {
+      cout << "Queue Underflow" << endl;
+      return;
+    } else if (front == rear) {
+      // empty case or removing single element case
+      arr[front] = -1;
+      rear = -1;
+      front = -1;
+    } else {
+      arr[front] = -1;
+      front++;
+    }
+  }
+
+  bool isEmpty() {
+    if (front == -1 && rear == -1) {
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+
+  int getSize() { return rear - front + 1; }
+
+  int getFront() {
+    if (front == -1) {
+      cout << "No element in Queue" << endl;
+      return -1;
+    } else {
+      return arr[front];
+    }
+  }
+
+  int getRear() {
+    if (rear == -1) {
+      cout << "No element in Queue" << endl;
+      return -1;
+    } else {
+      return arr[rear];
+    }
+  }
+
+  void printQueue() {
+    for (int i = 0; i < size; i++) {
+      cout << arr[i] << " ";
+    }
+    cout << endl;
+  }
+};
+
 int main() {
   queue< int > q;
 
@@ -22,4 +107,53 @@ int main() {
   q.push(50);
 
   cout << "Queue front: " << q.front() << " back: " << q.back() << endl;
+
+  cout << endl << "Custom Queue" << endl;
+
+  CustomQueueUsingArray que(10);
+  cout << "Size: " << que.getSize() << endl;
+
+  cout << "Front: " << que.getFront() << " Back: " << que.getRear() << endl
+       << endl;
+
+  que.push(11);
+  cout << "Front: " << que.getFront() << " Back: " << que.getRear() << endl;
+  cout << "Size: " << que.getSize() << endl;
+
+  que.push(12);
+  que.push(13);
+  que.push(14);
+  que.push(15);
+  que.push(16);
+  que.push(17);
+  que.push(18);
+  que.push(19);
+  que.push(20);
+
+  // overflow
+  que.push(21);
+
+  que.printQueue();
+  cout << "Size: " << que.getSize() << endl;
+
+  que.pop();
+
+  que.printQueue();
+  cout << "Size: " << que.getSize() << endl;
+
+  cout << "Front: " << que.getFront() << " Back: " << que.getRear() << endl
+       << endl;
+
+  que.pop();
+  que.pop();
+  que.pop();
+  que.pop();
+  que.pop();
+  que.pop();
+  que.pop();
+  que.pop();
+  que.pop();
+  que.pop();
+  cout << "Front: " << que.getFront() << " Back: " << que.getRear() << endl;
+  cout << "Size: " << que.getSize() << endl;
 }
---
+++ queues/readme.md
@@ -0,0 +1,27 @@
+# Queues
+
+- Queue follows FIFO (first in first out) Ordering.
+- Queue can be accessed via STL as well.
+- Some in built functions are `.front()`, `.back()`, `.size()`, `.empty`.
+
+## Custom Queue notes
+
+- We require two pointers front and back.
+- Consider overflow and underflow.
+- Push operation -> increment rear and insert at rear position.
+- Handle first element cases for push and pop.
+- First element case in push increment both front and rear.
+- Overflow push case when `rear = size - 1`.
+- Underflow pop case when `front == -1 && rear == -1`
+- Pop Operation `arr[front] = -1; front++;`
+- When `front == rear` single element is present in the Queue.
+- When single element is popped `front = -1; rear = -1`.
+- Size of Queue `int size = rear - front + 1`;
+- Empty Check `rear == -1; front == -1`.
+- Queue full check `size - rear == 0 ? true : false`.
+- Get front and back with valid index check.
+
+## Notes
+
+- Push will happen at rear/back.
+- Pop will happen at front.
