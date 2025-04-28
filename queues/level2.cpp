0d974a80793cb7585812b49641fbaafdfdf8a0e8
rajathongal <reachme.rajathongal@gmail.com>
2025-04-28 22:59:33 +0530

    queue level 2 completed with find first negative elem in k window prob

--- queues/level2.cpp
+++ queues/level2.cpp
@@ -102,6 +102,55 @@ void interLeaveQueue(queue< int > &first) {
   }
 }
 
+// first negative integer in every window of k
+// n-k+1 windoes exists
+// by brute force approach time complexity will be O(n * k)
+// Linear approach
+void printFirstNegativeInKWindow(int *arr, int n, int k) {
+  deque< int > q;
+
+  // process first window
+  for (int i = 0; i < k; i++) {
+    int element = arr[i];
+    if (element < 0) {
+      q.push_back(i);
+    }
+  }
+
+  // process remaining windows
+  // removal and addition of new elements as we move to new window
+  for (int i = k; i < n; i++) {
+    // previous answer
+    if (q.empty()) {
+      cout << "0 " << endl;
+    } else {
+      cout << arr[q.front()] << " ";
+    }
+
+    // Removal
+    // index - q.front >= k (out of window)
+    // (== k front of window)
+    // (<k second or other element in window)
+    if (i - q.front() >= k) {
+      // out of window range element
+      q.pop_front();
+    }
+
+    // addition
+    if (arr[i] < 0) {
+      q.push_back(i);
+    }
+  }
+
+  if (q.empty()) {
+    cout << "0 " << endl;
+  } else {
+    cout << arr[q.front()] << " ";
+  }
+
+  cout << endl;
+}
+
 int main() {
 
   // Uncomment for Reverse Queue using stack
@@ -129,17 +178,20 @@ int main() {
   // printNQueue(q);
 
   // Uncomment for interleave queue
-  queue< int > q;
-  q.push(10);
-  q.push(20);
-  q.push(30);
-  q.push(40);
-  q.push(50);
-  q.push(60);
-  q.push(70);
-  printNQueue(q);
-  interLeaveQueue(q);
-  printNQueue(q);
+  // queue< int > q;
+  // q.push(10);
+  // q.push(20);
+  // q.push(30);
+  // q.push(40);
+  // q.push(50);
+  // q.push(60);
+  // q.push(70);
+  // printNQueue(q);
+  // interLeaveQueue(q);
+  // printNQueue(q);
 
+  // Uncomment for first negative element in k window
+  int arr[] = {2, -5, 4, -1, -2, 0, 5};
+  printFirstNegativeInKWindow(arr, 7, 3);
   return 0;
 }
