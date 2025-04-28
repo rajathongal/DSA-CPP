bda8e5ec4755fa6ebf571ae15681abcff0f71860
rajathongal <reachme.rajathongal@gmail.com>
2025-04-28 21:42:15 +0530

    queue level 2 interleave q

--- queues/level2.cpp
+++ queues/level2.cpp
@@ -49,7 +49,8 @@ void reverseQueueUsingRecursion(queue< int > &q) {
 // Reverse K elements
 void reverseKElements(queue< int > &q, int &k) {
 
-  if(k > q.size() || k == 0) return;
+  if (k > q.size() || k == 0)
+    return;
 
   // Using Stack
   stack< int > st;
@@ -78,10 +79,56 @@ void reverseKElements(queue< int > &q, int &k) {
 }
 
 // Interleave first and second half of Queue
+void interLeaveQueue(queue< int > &first) {
+  queue< int > second;
+
+  // push first half of queue in second q
+  int size = first.size();
+  for (int i = 0; i < size / 2; i++) {
+    int temp = first.front();
+    first.pop();
+    second.push(temp);
+  }
+
+  // merge both halfves
+  // into original q named first
+  for (int i = 0; i < size / 2; i++) {
+    int temp = second.front();
+    second.pop();
+    first.push(temp);
+    temp = first.front();
+    first.pop();
+    first.push(temp);
+  }
+}
 
 int main() {
 
   // Uncomment for Reverse Queue using stack
+  // queue< int > q;
+  // q.push(10);
+  // q.push(20);
+  // q.push(30);
+  // q.push(40);
+  // q.push(50);
+  // q.push(60);
+  // cout << q.front() << " " << q.back() << endl;
+  // printNQueue(q);
+
+  // reverseQueueUsingStack(q);
+
+  // cout << q.front() << " " << q.back() << endl;
+  // printNQueue(q);
+
+  // reverseQueueUsingRecursion(q);
+  // cout << q.front() << " " << q.back() << endl;
+  // printNQueue(q);
+
+  // int k = 4;
+  // reverseKElements(q, k);
+  // printNQueue(q);
+
+  // Uncomment for interleave queue
   queue< int > q;
   q.push(10);
   q.push(20);
@@ -89,20 +136,9 @@ int main() {
   q.push(40);
   q.push(50);
   q.push(60);
-  cout << q.front() << " " << q.back() << endl;
+  q.push(70);
   printNQueue(q);
-
-  reverseQueueUsingStack(q);
-
-  cout << q.front() << " " << q.back() << endl;
-  printNQueue(q);
-
-  reverseQueueUsingRecursion(q);
-  cout << q.front() << " " << q.back() << endl;
-  printNQueue(q);
-
-  int k = 4;
-  reverseKElements(q, k);
+  interLeaveQueue(q);
   printNQueue(q);
 
   return 0;
