98a2cde8fd44ce8c702ad3af723e31339f1e8604
rajathongal <reachme.rajathongal@gmail.com>
2025-05-14 09:26:36 +0530

    leetcode 225 stacks using queues added

--- queues/assignments.cpp
+++ queues/assignments.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <stack>
 using namespace std;
 
@@ -81,36 +82,117 @@ public:
   bool empty() { return (s1.empty() && s2.empty()); }
 };
 
+// Leetcode 225: Implement stacks using queues
+// Method 1
+// Using 2 Queues and push is a heavy op rest are O(1)
+class MyStackM1 {
+  queue< int > q1;
+  queue< int > q2;
+
+public:
+  MyStackM1() {}
+
+  void push(int x) {
+    q2.push(x);
+
+    while (!q1.empty()) {
+      q2.push(q1.front());
+      q1.pop();
+    }
+
+    while (!q2.empty()) {
+      q1.push(q2.front());
+      q2.pop();
+    }
+  }
+
+  int pop() {
+    int poppedElem = q1.front();
+    q1.pop();
+    return poppedElem;
+  }
+
+  int top() { return q1.front(); }
+
+  bool empty() { return (q1.empty() && q2.empty()); }
+};
+
+// Method 2
+// Using only one queue
+class MyStackM2 {
+  queue< int > q;
+
+public:
+  MyStackM2() {}
+
+  void push(int x) {
+    q.push(x);
+    // reverse queue
+    for (int i = 0; i < q.size() - 1; i++) { // O(N)
+      int popElem = q.front();
+      q.pop();
+      q.push(popElem);
+    }
+  }
+
+  int pop() {
+    int popElem = q.front();
+    q.pop();
+    return popElem;
+  }
+
+  int top() { return q.front(); }
+
+  bool empty() { return q.empty(); }
+};
+
 int main() {
 
   // Uncomment for LC 232
   // M1
-  MyQueueM1 *obj = new MyQueueM1();
-  obj->push(1);
-  obj->push(2);
-  obj->push(3);
-  obj->push(4);
-  obj->push(5);
-  int param_2 = obj->pop();
-  cout << param_2 << endl;
-  int param_3 = obj->peek();
-  cout << param_3 << endl;
-  bool param_4 = obj->empty();
-  cout << param_4 << endl << endl;
+  // MyQueueM1 *obj = new MyQueueM1();
+  // obj->push(1);
+  // obj->push(2);
+  // obj->push(3);
+  // obj->push(4);
+  // obj->push(5);
+  // int param_2 = obj->pop();
+  // cout << param_2 << endl;
+  // int param_3 = obj->peek();
+  // cout << param_3 << endl;
+  // bool param_4 = obj->empty();
+  // cout << param_4 << endl << endl;
+
+  // // M2
+  // MyQueueM2 *obj2 = new MyQueueM2();
+  // obj2->push(1);
+  // obj2->push(2);
+  // obj2->push(3);
+  // obj2->push(4);
+  // obj2->push(5);
+  // int param_2_2 = obj2->pop();
+  // cout << param_2_2 << endl;
+  // int param_3_2 = obj2->peek();
+  // cout << param_3_2 << endl;
+  // bool param_4_2 = obj2->empty();
+  // cout << param_4_2 << endl;
+
+  // Uncomment for Leetcode 225 Implement stacks using queues
+  // M1
+  // MyStackM1 *myStack = new MyStackM1();
+  // myStack->push(1);
+  // myStack->push(2);
+  // cout << myStack->top() << endl;   // return 2
+  // cout << myStack->pop() << endl;   // return 2
+  // cout << myStack->empty() << endl; // return False
 
   // M2
-  MyQueueM2 *obj2 = new MyQueueM2();
-  obj2->push(1);
-  obj2->push(2);
-  obj2->push(3);
-  obj2->push(4);
-  obj2->push(5);
-  int param_2_2 = obj2->pop();
-  cout << param_2_2 << endl;
-  int param_3_2 = obj2->peek();
-  cout << param_3_2 << endl;
-  bool param_4_2 = obj2->empty();
-  cout << param_4_2 << endl;
+  MyStackM2 *myStack = new MyStackM2();
+  myStack->push(1);
+  myStack->push(2);
+  cout << myStack->top() << endl;   // return 2
+  cout << myStack->pop() << endl;   // return 2
+  cout << myStack->empty() << endl; // return False
 
   return 0;
 }
